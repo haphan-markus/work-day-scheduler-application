57cfbbe66662fd729e872aed1df69586721648b3
Ha Phan <ha.markus.phan@gmail.com>
2023-11-12 20:46:05 +0000

    assign color to time block based on real time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,5 @@
-// * Display the current day at the top of the calender when a user opens the planner.
-// There is a p with the id="currentDay" in the header, such as Thursday, September 5th, 2023
 var currentDay = document.getElementById("currentDay");
 currentDay.innerHTML = dayjs().format("dddd, MMMM D") + nth(dayjs()) + " " + dayjs().format("YYYY");
-
 
 var am9 = document.getElementById('09');
 var am10 = document.getElementById('10');
@@ -14,12 +11,26 @@ var pm3 = document.getElementById('15');
 var pm4 = document.getElementById('16');
 var pm5 = document.getElementById('17');
 
-console.log(am9);
-console.log(pm5);
+var textArea = document.querySelectorAll('textarea');
+
+textArea.forEach(function(elem){
+    let value =  parseInt(elem.id);
+    console.log(value);
+    let textBlock = document.getElementById(elem.id);
+    if (value < parseInt(dayjs().format("HH"))) {
+        textBlock.classList.add("past");
+    } else if (value > parseInt(dayjs().format("HH"))){
+        textBlock.classList.add("future");
+    } else {
+        textBlock.classList.add("present");
+    }
+})
+console.log(textArea[0].id);
 
 console.log(dayjs().format("HH"));
 console.log(typeof(dayjs().format("HH"))); // Return as a string
 
+// am9.classList.add("future");
 // If the time is smaller than the real time, then put the class of past, present, future.
  
 // * Color-code each timeblock based on past, present, and future when the timeblock is viewed.
