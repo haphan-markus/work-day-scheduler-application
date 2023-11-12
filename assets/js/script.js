6154822d0ea4152f0bb903b75b0457541fbece6c
Ha Phan <ha.markus.phan@gmail.com>
2023-11-12 18:13:15 +0000

    work on colour pattern based on the current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,33 @@
 var currentDay = document.getElementById("currentDay");
 currentDay.innerHTML = dayjs().format("dddd, MMMM D") + nth(dayjs()) + " " + dayjs().format("YYYY");
 
+
+var am9 = document.getElementById('09');
+var am10 = document.getElementById('10');
+var am11 = document.getElementById('11');
+var pm12 = document.getElementById('12');
+var pm1 = document.getElementById('13');
+var pm2 = document.getElementById('14');
+var pm3 = document.getElementById('15');
+var pm4 = document.getElementById('16');
+var pm5 = document.getElementById('17');
+
+console.log(am9);
+console.log(pm5);
+
+console.log(dayjs().format("HH"));
+console.log(typeof(dayjs().format("HH"))); // Return as a string
+
+// If the time is smaller than the real time, then put the class of past, present, future.
+ 
+// * Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+ 
+// * Allow a user to enter an event when they click a timeblock
+
+// * Save the event in local storage when the save button is clicked in that timeblock.
+
+// * Persist events between refreshes of a page
+
 function nth(i){
     if ((i > 3) || (i < 21)){
         return "th";
@@ -16,16 +43,4 @@ function nth(i){
     }
 }
 
-
 console.log(nth(dayjs()));
-
-
-// * Present timeblocks for standard business hours when the user scrolls down.
- 
-// * Color-code each timeblock based on past, present, and future when the timeblock is viewed.
- 
-// * Allow a user to enter an event when they click a timeblock
-
-// * Save the event in local storage when the save button is clicked in that timeblock.
-
-// * Persist events between refreshes of a page
